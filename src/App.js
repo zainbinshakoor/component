a6dbd49e94153865b3833f46d6f8e397ef9cc206
yammybajwa <YAMMYBAJWA09@GMAIL.COM>
2022-02-24 21:27:30 +0500

    state method and using class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+class App extends Component{
+constructor(){
+  super();
+  this.state={
+    String:"Hello Zain",
+    
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            {this.state.String}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button onClick={()=>this.setState({String:"New Genration"})}> ONCLICK</button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
